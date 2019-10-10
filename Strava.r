b7d0a24da94696263a626040e5a0d02410c5aeaf
Frank Neugebauer <43097727+neuggs@users.noreply.github.com>
2019-10-10 10:39:09 -0400

    Cleanup so basic functionality works again.

--- Strava.r
+++ Strava.r
@@ -2,18 +2,16 @@
 library(xlsx)
 library(broom)
 library(psych)
-
 library("ggplot2")
 
 # All data
 data_types <- c('character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'character', 
-                'character', 'character', 'character', 'character', 'character', 'numeric', 'numeric', 'numeric',
-                'numeric', 'numeric', 'numeric')
+                'character', 'character', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric', 'numeric')
 
 strava_data <- read.xlsx("./all_data.xlsx", 1, colClasses = data_types, header=TRUE)
 
 # Only ride data
-ride_data <- subset(strava_data, type == 'VirtualRide' | type == 'Ride')
+ride_data <- subset(strava_data, (type == 'VirtualRide' | type == 'Ride') & average_watts > 0)
 
 assumptions <- function(field_label, data, field, bins) {
   gg <- ggplot(data, aes(field)) + geom_histogram(bins=bins) + 
@@ -57,16 +55,13 @@ a <- assumptions('Moving Time (Minutes)', ride_data, ride_data$moving_time, 15)
 a <- assumptions('Elevation Gain (Feet)', ride_data, ride_data$elevation_gain_ft, 15)
 a <- assumptions('Average Power (Watts)', ride_data, ride_data$average_watts, 15)
 a <- assumptions('Average Heart Rate', ride_data, ride_data$average_heartrate, 15)
-a <- assumptions('Average Temperature (Celsius)', ride_data, ride_data$average_temp, 15)
  
 
 ggplot(ride_data, aes(x = distance_mi, y = average_speed_mph), color = factor(gear_id)) + 
       geom_point()
  
-cor(some_bikes$distance_mi, strava_data$average_speed_mph)
- 
-# boxplot with just some bikes
-ggplot(data = some_bikes, aes(x = factor(gear_id), y = average_speed_mph)) +
+# boxplot with bikes
+ggplot(data = ride_data, aes(x = factor(gear_id), y = average_speed_mph)) +
         geom_boxplot()
  
 # Simple lm model - how distance affects speed
@@ -83,6 +78,4 @@ lm_plot <- ggplot(augmented_bikes, aes(x = distance_mi, y = average_speed_mph, c
 # single call to geom_line()
 lm_plot + geom_line(aes(y = .fitted))
 lm_plot.Show()
- 
-summary(logistic_model)
-model_chi <- logistic_model.1$null.deviance - logistic_model.1$deviance
+
